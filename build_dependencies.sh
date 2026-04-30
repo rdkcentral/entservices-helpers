ed12622c6c17a7a1f8d73d8744ea6438a3e53d29
Preeja Raveendran <preeja_raveendran@comcast.com>
2026-04-30 08:08:19 +0530

    copying workflow

---
+++ build_dependencies.sh
@@ -0,0 +1,107 @@
+#!/bin/bash
+#
+# If not stated otherwise in this file or this component's LICENSE
+# file the following copyright and licenses apply:
+#
+# Copyright 2026 RDK Management
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+set -x
+set -e
+##############################
+GITHUB_WORKSPACE="${PWD}"
+ls -la ${GITHUB_WORKSPACE}
+cd ${GITHUB_WORKSPACE}
+
+# # ############################# 
+#1. Install Dependencies and packages
+
+apt update
+apt install -y libcurl4-openssl-dev valgrind lcov clang libsystemd-dev meson curl libunwind-dev libgstreamer1.0-dev libgstreamer-plugins-base1.0-dev
+pip install jsonref
+
+############################
+# Build trevor-base64
+if [ ! -d "trower-base64" ]; then
+git clone https://github.com/xmidt-org/trower-base64.git
+fi
+cd trower-base64
+meson setup --warnlevel 3 --werror build
+ninja -C build
+ninja -C build install
+cd ..
+###########################################
+# Clone the required repositories
+
+
+git clone --branch  R4.4.3 https://github.com/rdkcentral/ThunderTools.git
+
+git clone --branch R4.4.1 https://github.com/rdkcentral/Thunder.git
+
+git clone --branch develop https://github.com/rdkcentral/entservices-testframework.git
+
+############################
+# Build Thunder-Tools
+echo "======================================================================================"
+echo "buliding thunderTools"
+cd ThunderTools
+patch -p1 < $GITHUB_WORKSPACE/entservices-testframework/patches/00010-R4.4-Add-support-for-project-dir.patch
+cd -
+
+
+cmake -G Ninja -S ThunderTools -B build/ThunderTools \
+    -DEXCEPTIONS_ENABLE=ON \
+    -DCMAKE_INSTALL_PREFIX="$GITHUB_WORKSPACE/install/usr" \
+    -DCMAKE_MODULE_PATH="$GITHUB_WORKSPACE/install/tools/cmake" \
+    -DGENERIC_CMAKE_MODULE_PATH="$GITHUB_WORKSPACE/install/tools/cmake" \
+
+cmake --build build/ThunderTools --target install
+
+
+############################
+# Build Thunder
+echo "======================================================================================"
+echo "buliding thunder"
+
+cd Thunder
+patch -p1 < $GITHUB_WORKSPACE/entservices-testframework/patches/Use_Legact_Alt_Based_On_ThunderTools_R4.4.3.patch
+patch -p1 < $GITHUB_WORKSPACE/entservices-testframework/patches/error_code_R4_4.patch
+patch -p1 < $GITHUB_WORKSPACE/entservices-testframework/patches/1004-Add-support-for-project-dir.patch
+patch -p1 < $GITHUB_WORKSPACE/entservices-testframework/patches/RDKEMW-733-Add-ENTOS-IDS.patch
+patch -p1 < $GITHUB_WORKSPACE/entservices-testframework/patches/Jsonrpc_dynamic_error_handling.patch
+cd -
+
+cmake -G Ninja -S Thunder -B build/Thunder \
+    -DMESSAGING=ON \
+    -DCMAKE_INSTALL_PREFIX="$GITHUB_WORKSPACE/install/usr" \
+    -DCMAKE_MODULE_PATH="$GITHUB_WORKSPACE/install/tools/cmake" \
+    -DGENERIC_CMAKE_MODULE_PATH="$GITHUB_WORKSPACE/install/tools/cmake" \
+    -DBUILD_TYPE=Debug \
+    -DBINDING=127.0.0.1 \
+    -DPORT=55555 \
+    -DEXCEPTIONS_ENABLE=ON \
+
+cmake --build build/Thunder --target install
+
+############################
+# generating minimal mock headers
+cd $GITHUB_WORKSPACE/entservices-testframework/Tests
+mkdir -p headers
+cd headers
+touch secure_wrapper.h
+touch wpa_ctrl.h
+touch rdk_logger_milestone.h
+cd $GITHUB_WORKSPACE
+
+ls -la ${GITHUB_WORKSPACE}
---
+++ services.cmake
@@ -0,0 +1,70 @@
+# If not stated otherwise in this file or this component's Licenses.txt file the
+# following copyright and licenses apply:
+#
+# Copyright 2016 RDK Management
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+#
+# features
+#
+
+add_definitions (-DUSE_IARM)
+option(USE_IARM "USE_IARM" ON)
+
+add_definitions (-DUSE_IARM_BUS)
+option(USE_IARM_BUS "USE_IARM_BUS" ON)
+
+add_definitions (-DUSE_IARMBUS)
+
+add_definitions (-DUSE_TR_69)
+add_definitions (-DHAS_API_SYSTEM)
+add_definitions(-DRDK_LOG_MILESTONE)
+
+add_definitions (-DUSE_DS)
+
+option(PLUGIN_HELPERS "PLUGIN_HELPERS" ON)
+option(PLUGIN_TELEMETRY "PLUGIN_TELEMETRY" ON)
+option(PLUGIN_CONTINUEWATCHING "PLUGIN_CONTINUEWATCHING" ON)
+
+add_definitions (-DPLUGIN_CONTINUEWATCHING)
+option(PLUGIN_CONTINUEWATCHING "PLUGIN_CONTINUEWATCHING" ON)
+
+if(PLUGIN_CONTINUEWATCHING)
+    if(CONTINUEWATCHING_DISABLE_SECAPI)
+        add_definitions (-DDISABLE_SECAPI)
+    endif()
+endif()
+
+if (BUILD_DBUS)
+    message("Building for DBUS")
+
+    add_definitions (-DBUILD_DBUS)
+    option(BUILD_DBUS "BUILD_DBUS" ON)
+    add_definitions (-DIARM_USE_DBUS)
+    option(IARM_USE_DBUS "IARM_USE_DBUS" ON)
+endif()
+
+if (BUILD_ENABLE_TELEMETRY_LOGGING)
+    message("Building with telemetry logging")
+    add_definitions (-DENABLE_TELEMETRY_LOGGING)
+endif()
+
+if (ENABLE_RFC_MANAGER)
+    message("Using binary for RFC Maintenance task")
+    add_definitions (-DENABLE_RFC_MANAGER=ON)
+endif()
+
+if(BUILD_ENABLE_ERM)
+        add_definitions(-DENABLE_ERM)
+endif()
