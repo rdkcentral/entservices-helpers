c890710e373d4a0708e14ec2ae4ae7c4a731868a
Preeja Raveendran <preeja_raveendran@comcast.com>
2026-05-23 22:41:39 +0530

    utils.cpp removed

--- helpers/utils.h
+++ helpers/utils.h
@@ -31,11 +31,14 @@
 #include "UtilsString.h"
 #include "UtilsfileExists.h"
 #include "UtilsgetFileContent.h"
+#include <curl/curl.h>
+#include <mutex>
 
 // std
 #include <string>
 
 #define SERVER_DETAILS  "127.0.0.1:9998"
+#define MAX_STRING_LENGTH 2048
 
 #define UNUSED(expr)(void)(expr)
 #define C_STR(x) (x).c_str()
@@ -182,24 +185,152 @@ template<typename T> inline T clamp(T x, T lo, T hi) { return max(min(x, hi), lo
         param = parameters[paramName].String();\
 }
 
+using namespace WPEFramework;
+using namespace std;
+
 namespace Helpers
 {
 
+    static inline size_t writeCurlResponse(void *ptr, size_t size, size_t nmemb, string stream)
+    {
+        size_t realsize = size * nmemb;
+        string temp(static_cast<const char*>(ptr), realsize);
+        stream.append(temp);
+        return realsize;
+    }
+
     struct SecurityToken
     {
-        static bool isThunderSecurityConfigured();
+        static inline bool isThunderSecurityConfigured()
+        {
+            bool configured = false;
+            long http_code = 0;
+            std::string jsonResp;
+            CURL *curl_handle = NULL;
+            CURLcode res = CURLE_OK;
+            curl_handle = curl_easy_init();
+            string serialNumber = "";
+            string url = "http://127.0.0.1:9998/Service/Controller/Configuration/Controller";
+            if (curl_handle &&
+                !curl_easy_setopt(curl_handle, CURLOPT_URL, url.c_str()) &&
+                !curl_easy_setopt(curl_handle, CURLOPT_HTTPGET,1) &&
+                !curl_easy_setopt(curl_handle, CURLOPT_FOLLOWLOCATION, 1) && //when redirected, follow the redirections
+                !curl_easy_setopt(curl_handle, CURLOPT_WRITEFUNCTION, writeCurlResponse) &&
+                !curl_easy_setopt(curl_handle, CURLOPT_WRITEDATA, &jsonResp)) {
+
+                res = curl_easy_perform(curl_handle);
+                if(curl_easy_getinfo(curl_handle, CURLINFO_RESPONSE_CODE, &http_code) != CURLE_OK)
+                {
+                    std::cout << "curl_easy_getinfo failed\n";
+                }
+                std::cout << "Thunder Controller Configuration ret: " << res << " http response code: " << http_code << std::endl;
+            }
+            else
+            {
+                std::cout << "Could not perform curl to read Thunder Controller Configuration\n";
+            }
+
+            if (curl_handle) {
+                curl_easy_cleanup(curl_handle);
+            }
+            if ((res == CURLE_OK) && (http_code == 200))
+            {
+                //check for "Security" in response
+                JsonObject responseJson = JsonObject(jsonResp);
+                if (responseJson.HasLabel("subsystems"))
+                {
+                    const JsonArray subsystemList = responseJson["subsystems"].Array();
+                    for (int i=0; i<subsystemList.Length(); i++)
+                    {
+                        string subsystem = subsystemList[i].String();
+                        if (subsystem == "Security")
+                        {
+                            configured = true;
+                            break;
+                        }
+                    }
+                }
+            }
+            return configured;
+        }
     };
 
 #ifdef USE_THUNDER_COMMUNICATION
     // Thunder Plugin Communication
-    std::shared_ptr<WPEFramework::JSONRPC::LinkType<WPEFramework::Core::JSON::IElement>> getThunderControllerClient(std::string callsign="");
+    inline std::shared_ptr<WPEFramework::JSONRPC::LinkType<WPEFramework::Core::JSON::IElement> > getThunderControllerClient(std::string callsign="")
+    {
+        Core::SystemInfo::SetEnvironment(_T("THUNDER_ACCESS"), (_T(SERVER_DETAILS)));
+        std::shared_ptr<WPEFramework::JSONRPC::LinkType<WPEFramework::Core::JSON::IElement> > thunderClient = make_shared<WPEFramework::JSONRPC::LinkType<WPEFramework::Core::JSON::IElement> >(callsign.c_str(), "",false);
 
-    void activatePlugin(const char* callSign);
+        return thunderClient;
+    }
 
-    bool isPluginActivated(const char* callSign);
+    inline bool isPluginActivated(const char* callSign)
+    {
+        string method = "status@" + string(callSign);
+        Core::JSON::ArrayType<PluginHost::MetaData::Service> joResult;
+        uint32_t status = getThunderControllerClient()->Get<Core::JSON::ArrayType<PluginHost::MetaData::Service> >(2000, method.c_str(),joResult);
+        bool pluginActivated = false;
+        if (status == Core::ERROR_NONE)
+        {
+            LOGINFO("Getting status for callSign %s, result: %s", callSign, joResult[0].JSONState.Data().c_str());
+            pluginActivated = joResult[0].JSONState == PluginHost::IShell::ACTIVATED;
+        }
+        else
+        {
+            LOGWARN("Getting status for callSign %s, status: %d", callSign, status);
+        }
+
+        if(!pluginActivated){
+            LOGWARN("Plugin %s is not active", callSign);
+        } else {
+            LOGINFO("Plugin %s is active ", callSign);
+        }
+        return pluginActivated;
+    }
+
+    inline void activatePlugin(const char* callSign)
+    {
+        JsonObject joParams;
+        joParams.Set("callsign",callSign);
+        JsonObject joResult;
+
+        if(!isPluginActivated(callSign))
+        {
+            LOGINFO("Activating %s", callSign);
+            uint32_t status = getThunderControllerClient()->Invoke<JsonObject, JsonObject>(2000, "activate", joParams, joResult);
+            string strParams;
+            string strResult;
+            joParams.ToString(strParams);
+            joResult.ToString(strResult);
+            LOGINFO("Called method %s, with params %s, status: %d, result: %s"
+                    , "activate"
+                    , C_STR(strParams)
+                    , status
+                    , C_STR(strResult));
+            if (status == Core::ERROR_NONE)
+            {
+                LOGINFO("%s Plugin activation status ret: %d ", callSign, status);
+            }
+        }
+    }
+
 #endif
 
 #ifndef DONT_USE_TR181
-    bool getTR181Config(const char* callerID, const char* paramName, TR181_ParamData_t& paramOutput);
+    inline bool getTR181Config(const char* callerID, const char* paramName, TR181_ParamData_t& paramOutput)
+    {
+        tr181ErrorCode_t status;
+
+        memset(&paramOutput, 0, sizeof(paramOutput));
+        status = getParam((char*)callerID, (char*)paramName, &paramOutput);
+        if (tr181Success == status )
+        {
+            std::cout << "getTR181Config for  " << paramName << " is " << paramOutput.value << std::endl;
+        } else {
+            std::cout << "Could not get param value for " << paramName  << std::endl;
+        }
+        return (tr181Success == status );
+    }
 #endif
 }
